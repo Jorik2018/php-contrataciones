a704922923a29034edb4eb5b5dd91d9f80574316
Jorik2018 <antoniopinedo2018@gmail.com>
2025-04-14 08:08:57 -0500

    x

--- finalizadas-2025/index.php
+++ finalizadas-2025/index.php
@@ -7,23 +7,7 @@
 
 	<a class="skip-link screen-reader-text" href="https://www.senace.gob.pe/contrataciones/finalizadas-2014/#wp--skip-link--target">Saltar al contenido</a>
 	<div class="wp-site-blocks">
-		<header class="wp-block-template-part">
-			<div class="wp-block-group gapless-group has-vivid-red-background-color has-background is-layout-constrained wp-block-group-is-layout-constrained" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">
-				<div class="wp-block-group is-layout-flow wp-block-group-is-layout-flow">
-					<div class="wp-block-group alignfull site-header is-content-justification-space-between is-layout-flex wp-container-core-group-layout-3 wp-block-group-is-layout-flex" style="padding-top:var(--wp--custom--gap--vertical);padding-bottom:var(--wp--custom--gap--vertical)">
-						<div class="wp-block-group site-brand is-nowrap is-layout-flex wp-container-core-group-layout-2 wp-block-group-is-layout-flex">
-							<figure class="wp-block-image size-full is-resized"><img decoding="async" src="/contrataciones/logoGOB.svg" alt="" class="wp-image-71825" width="127"></figure>
-
-
-
-							<div class="wp-block-group is-layout-flow wp-container-core-group-layout-1 wp-block-group-is-layout-flow" style="border-left-color:var(--wp--preset--color--background);border-left-width:1px;padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:1rem">
-								<h1 class="wp-block-heading has-background-color has-text-color" style="font-size:15px;font-style:normal;font-weight:400">Senace – Servicio Nacional de Certificación Ambiental para las Inversiones Sostenibles</h1>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
+	<?php include '../header.php'; ?>
 
 
 		<div class="wp-block-group is-layout-constrained wp-block-group-is-layout-constrained" style="margin-bottom:var(--wp--preset--spacing--70)">
