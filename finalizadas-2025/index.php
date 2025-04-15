2a8a9ada8b570dc6c6ce66137d66322ba134df75
Troy Emerson Vega Tarazona <trvega@midis.gob.pe>
2025-04-15 16:47:11 -0500

    x

--- finalizadas-2025/index.php
+++ finalizadas-2025/index.php
@@ -62,7 +62,7 @@
 
 											<tr>
 												<td>
-													<p class="recruitment__number"> <strong>--</strong> </p>
+													<p class="recruitment__number"> <strong>   </strong> </p>
 												</td>
 												<td style="text-align: left; padding:0px 15px;"> <a class="abc" href="#" target="_blank" rel="noopener noreferrer"><strong>  </strong></a> <br>  </td>
 												<td style="text-align: left;">
