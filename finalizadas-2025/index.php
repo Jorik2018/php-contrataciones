edf4b37aeaf89729938b03dfb1ea5ad3d7d23d6a
Troy Emerson Vega Tarazona <trvega@midis.gob.pe>
2025-04-15 15:03:23 -0500

    x

--- finalizadas-2025/index.php
+++ finalizadas-2025/index.php
@@ -41,7 +41,7 @@
 												<td>
 													<p class="recruitment__number"> <strong>--</strong> </p>
 												</td>
-												<td style="text-align: left; padding:0px 15px;"> <a class="abc" href="#" target="_blank" rel="noopener noreferrer"><strong>--</strong></a> <br> (1 vacante) </td>
+												<td style="text-align: left; padding:0px 15px;"> <a class="abc" href="#" target="_blank" rel="noopener noreferrer"><strong>--</strong></a> <br> -- </td>
 												<td style="text-align: left;">
 													<ul>
 														<li> 
