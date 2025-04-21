dc6e2965c6b26abb3446dbba29ab855a3ae6b463
Troy Emerson Vega Tarazona <trvega@midis.gob.pe>
2025-04-21 07:55:45 -0500

    x

--- vigentes/index.php
+++ vigentes/index.php
@@ -78,6 +78,26 @@
 										<td style="text-align: left;"> </td>
 									</tr>
 
+									<tr>
+										<td> 
+											<br>
+											 
+										</td>
+										<td style="text-align: left; padding:0px 15px;"> <a class="abc" href="#" target="_blank" rel="noopener noreferrer"><strong>   </strong></a> <br>  </td>
+										<td style="text-align: left; padding:0px 15px;"> <a class="abc" href="#" target="_blank" rel="noopener noreferrer"><strong>   </strong></a> <br>  </td>
+										<td style="text-align: left;"> </td>
+									</tr>
+
+									<tr>
+										<td> 
+											<br>
+											 
+										</td>
+										<td style="text-align: left; padding:0px 15px;"> <a class="abc" href="#" target="_blank" rel="noopener noreferrer"><strong>   </strong></a> <br>  </td>
+										<td style="text-align: left; padding:0px 15px;"> <a class="abc" href="#" target="_blank" rel="noopener noreferrer"><strong>   </strong></a> <br>  </td>
+										<td style="text-align: left;"> </td>
+									</tr>
+
 								</tbody>
 							</table>
 						</div>
