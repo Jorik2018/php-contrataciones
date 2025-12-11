0e2934a3067f5b523730969fd4f8eb308a028491
jorik2018 <antoniopinedo2018@gmail.com>
2025-12-11 18:24:34 -0500

    x

--- index.php
+++ index.php
@@ -203,6 +203,11 @@ $years=$VistaModel::getYears(null);
 
         <!-- Scripts -->
         <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+        <link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+      integrity="sha512-u5EdW8PhkH6Q0xTmvQG8sF7k5eagKJx1CpxF7fF0uH7hQxZZgwyXR0Yy+IRx5g71D6nox38Z4Z8PxxCuUCrPNA=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
