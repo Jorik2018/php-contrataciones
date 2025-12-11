428f41d5decded9c214610b0890f3d5ae582ccc4
jorik2018 <antoniopinedo2018@gmail.com>
2025-12-11 15:19:08 -0500

    x

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
  require_once ( __DIR__ ."/models/vista.php");
  $VistaModel = new Vista();
-$years=$VistaModel::getYears();
+$years=$VistaModel::getYears(null);
  foreach ($years as $item){
 ?>
     <li>
