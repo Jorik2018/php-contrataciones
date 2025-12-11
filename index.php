aec3682d1b2549ac90dc87a65485390a75587c74
jorik2018 <antoniopinedo2018@gmail.com>
2025-12-11 14:56:15 -0500

    x

--- index.php
+++ index.php
@@ -93,8 +93,8 @@
  <li class="nav-item">
 
  <?
- require_once "../models/vista.php";
- echo json_encode($VistaModel::getYears($_GET["status"]));
+ require_once "/models/vista.php";
+ //echo json_encode($VistaModel::getYears($_GET["status"]));
  ?>
                             <a class="nav-link active" href="?status=C">Convocatorias Concluidas</a>
                         </li>
