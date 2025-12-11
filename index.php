c778bf2053d5028167e11cea32d8489e7390cee0
jorik2018 <antoniopinedo2018@gmail.com>
2025-12-11 18:27:12 -0500

    x

--- index.php
+++ index.php
@@ -203,14 +203,14 @@ $years=$VistaModel::getYears(null);
 
         <!-- Scripts -->
         <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-        <link rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-      integrity="sha512-u5EdW8PhkH6Q0xTmvQG8sF7k5eagKJx1CpxF7fF0uH7hQxZZgwyXR0Yy+IRx5g71D6nox38Z4Z8PxxCuUCrPNA=="
-      crossorigin="anonymous" referrerpolicy="no-referrer" />
-
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+        <link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+      integrity="sha512-iN0NlpABlznrb74shArPnT3uFoxIjb+fJswurN0IkqK0WTXyLwS2IPUFKbZvFwZyM0CnNOCzEFPp9jayN8E2Ig=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.min.js" integrity="sha512-b+nQTCdtTBIRIbraqNEwsjB6UvL3UEMkXnhzd8awtCYh0Kcsjl9uEgwVFVbhoj3uu1DO1ZMacNvLoyJJiNfcvg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script>
             $(document).ready(function () {
