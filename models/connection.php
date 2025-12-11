2c847a62255ef749934db07d1942138303d7a050
jorik2018 <antoniopinedo2018@gmail.com>
2025-12-11 12:52:15 -0500

    x

--- models/connection.php
+++ models/connection.php
@@ -8,7 +8,7 @@ class Connection
 
     static public function infoDatabase()
     {
-        return include(__DIR__ . '/settings.php');
+        return include(__DIR__ . '../settings.php');
     }
 
     /**
--- models/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    "database" => "bwgvinpi_contrataciones",
-    "user" => "bwgvinpi_ext",
-    "pass" => "3r&4v7+3xD8I"
-];
